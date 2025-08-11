95708e53d2c2e7f33c68c53d57711bda6a8b92d4
Anushka Chavan <chavananushka2024@gmail.com>
2025-08-11 17:34:33 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const resolver = new Resolver();
 
 resolver.define('getText', (req) => {
   console.log(req);
-
+  console.log("hello world");
+  console.log("hello world 2");
+  console.log("hello world 3");
   return 'welcome';
   
 });
